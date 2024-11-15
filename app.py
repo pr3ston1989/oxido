4b4689ac7dcc234e4c61ef8b87007388ad103844
Damian Pietrzak <150837797+pr3ston1989@users.noreply.github.com>
2024-11-15 19:11:14 +0100

    Add OpenAI API integration.

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
 import requests
-
+import openai
+from openai import OpenAI
+import os
 
 article_url = 'https://cdn.oxido.pl/hr/Zadanie%20dla%20JJunior%20AI%20Developera%20-%20tresc%20artykulu.txt'
 
-ai_prompt = '''create html with proper tags to paste into body,
-            correct text, place img tags with captions and with
+ai_prompt = '''create article with proper tags ready to paste between body tags,
+            nothing more, fix encoding, place img tags with captions and with
             polish prompts in alt attributes to create accurate images,
-            as src leave "image_placeholder.jpg"'''
+            as src leave "image_placeholder.jpg" for text below:'''
 
 def load_article_data():
     response = requests.get(article_url)
@@ -17,19 +19,37 @@ def load_article_data():
     return content
 
 load_dotenv()
-
-# app = Flask(__name__)
-
-
-# @app.route("/")
-# def home():
-#     data = load_article_data()
-#     """Ścieżka do strony głównej - wyświetla stronę ze sformatowanym artykułem."""
-#     print(data)
-#     return render_template("index.html", data=data)
+API_KEY = os.getenv('API_KEY')
+client = OpenAI(api_key=API_KEY)
 
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000)
+def create_article(prompt, content):
+    article_html = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": f"{prompt} {content}"},
+        ],
+        )
+    return article_html.choices[0].message.content
 
-print(load_article_data())
+def save_as_html(data):
+    with open("./files/artykul.html", "w", encoding="utf-8") as file:
+        file.write(data)
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """Ścieżka do strony głównej - wyświetla stronę ze sformatowanym artykułem."""
+    article_data = load_article_data()
+    article = create_article(ai_prompt, article_data)
+    save_as_html(article)
+    return render_template("index.html", data=article)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
